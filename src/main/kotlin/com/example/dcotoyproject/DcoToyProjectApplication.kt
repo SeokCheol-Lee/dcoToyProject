fbc4a17cef08abe5f1369776ac0f9d1c5308896c
vamlin <lsc991123@gmail.com>
2025-05-25 16:20:34 +0900

    init

---
+++ src/main/kotlin/com/example/dcotoyproject/DcoToyProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dcotoyproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DcoToyProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<DcoToyProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dcotoyproject/DcoToyProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dcotoyproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DcoToyProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
